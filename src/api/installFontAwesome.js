869be56d070e3679bf5d60d69afc724ff5f365a0
Sai-Krishna-Web <60420794+Sai-Krishna-Web@users.noreply.github.com>
2020-04-30 22:00:09 +0530

    Add files via upload

---
+++ src/api/installFontAwesome.js
@@ -0,0 +1,28 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCircle, faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faComment } from '@fortawesome/free-regular-svg-icons';
+import {
+  faGithub,
+  faZhihu,
+  faFacebookF,
+  faFacebook,
+  faLinkedin,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
+
+const installFontAwesome = () => {
+  library.add(
+    faCircle,
+    faComment,
+    faChevronUp,
+    faEnvelope,
+    faGithub,
+    faZhihu,
+    faFacebookF,
+    faFacebook,
+    faLinkedin,
+    faTwitter
+  );
+};
+
+export default installFontAwesome;
---
+++ src/content/guestbook.md
@@ -0,0 +1,12 @@
+---
+templateKey: blog-post
+id: https://calpa.me/guestbook/
+title: Guestbook
+slug: /guestbook
+date: 2016-01-18T03:48:03.125Z
+description: ""
+tags:
+  - Reading
+---
+
+Welcome message
